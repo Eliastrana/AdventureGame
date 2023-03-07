4f0dec4341d12ee0ee61692a3e811ca55fa1f7db
eliastt <eliastt@stud.ntnu.no>
2023-03-07 11:15:02 +0100

    filled in the testclasses that are within action and goals

---
+++ src/test/java/edu/ntnu/idatt2001/Action/GoldActionTest.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2001.Action;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoldActionTest {
+
+    @Test
+    void execute() {
+    }
+}
---
+++ src/test/java/edu/ntnu/idatt2001/goals/HealthGoalTest.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2001.goals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthGoalTest {
+
+    @Test
+    void isFullfilled() {
+    }
+}
